1d5bf61b7b687f923b7dc7162d818108ac92516d
SilvaX96 <mattheus_psilva@outlook.com>
2025-06-04 15:50:02 +0000

    Desafio -  Xadrez Novato

---
+++ Desafio Xadrex.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    // Simulação do movimento da Torre usando o loop for
+    int casasTorre = 5;
+    printf("Movimento da Torre:\n");
+    for (int i = 1; i <= casasTorre; i++) {
+        printf("Direita\n");
+    }
+
+    // Simulação do movimento do Bispo usando o loop while
+    int casasBispo = 5;
+    int contadorBispo = 1;
+    printf("\nMovimento do Bispo:\n");
+    while (contadorBispo <= casasBispo) {
+        printf("Cima Direita\n");
+        contadorBispo++;
+    }
+
+    // Simulação do movimento da Rainha usando o loop do-while
+    int casasRainha = 8;
+    int contadorRainha = 1;
+    printf("\nMovimento da Rainha:\n");
+    do {
+        printf("Esquerda\n");
+        contadorRainha++;
+    } while (contadorRainha <= casasRainha);
+
+    return 0;
+}
